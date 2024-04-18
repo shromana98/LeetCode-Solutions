60cf71c1acba976c34efa9a9e187727dab8046d1
Shromana Majumder <115609505+shromana98@users.noreply.github.com>
2024-04-18 15:03:02 +0700

    Time: 1112 ms (100.00%), Space: 0B (100.00%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT person_name 
+FROM   (SELECT person_name, 
+               @accu := @accu + weight AS accu 
+        FROM   (SELECT person_name, weight
+                FROM   Queue 
+                ORDER  BY turn) q, 
+               (SELECT @accu := 0) vars) t 
+WHERE  accu <= 1000 
+ORDER  BY accu DESC 
+LIMIT  1;
